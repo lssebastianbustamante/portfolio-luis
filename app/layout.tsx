57571b001d668972c6f126dbda597913e156555c
Luis Bustamante <luis.bustamante@infracommerce.lat>
2025-04-15 14:49:21 -0300

    ajuste font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Comfortaa, Geist, Geist_Mono, Noto_Sans } from "next/font/google";
+import { Comfortaa, Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${comfortaa.variable} antialiased`}
+        className={`${noto.variable} ${comfortaa.variable} antialiased`}
       >
         {children}
       </body>
