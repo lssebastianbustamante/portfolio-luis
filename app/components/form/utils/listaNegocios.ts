c825b712130cd315cd2f46394387e2efcd45f912
Luis Bustamante <luissebastianbustamante@gmail.com>
2025-11-01 23:32:40 -0300

    chore(form): limpia utilidades y datos de geografía/negocios del formulario

--- app/components/form/utils/listaNegocios.ts
+++
@@ -1,9 +0,0 @@
-export const listadoTipoDeNegocios = (listaDeNegocios: string) => {
-
-  const listaDeNegociosArray = listaDeNegocios.split(',').map((negocio) => negocio.trim()).map((negocio) => ({
-    value: negocio.toLowerCase(),
-    label: negocio
-  }))
-
-  return listaDeNegociosArray
-}
